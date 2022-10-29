fcffa8df9d1b60744ec3db528d3c800d6999e6d4
Pawel K <pawelkarabowicz@gmail.com>
2022-10-29 20:00:13 +0200

    feat: Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import {AdminComponent} from "./admin/admin.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent},
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
