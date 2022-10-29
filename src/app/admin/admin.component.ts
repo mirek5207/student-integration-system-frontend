cbc0a66feba9a16c0ad8de7f5c86846ad4e96f70
Pawel K <pawelkarabowicz@gmail.com>
2022-10-29 20:11:57 +0200

    feat: Add admin component

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import {ModeratorInfo} from "../shared/interfaces/register.interface";
+import {AdminService} from "../shared/services/admin/admin.service";
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent implements OnInit {
+
+  registerNewModerator: ModeratorInfo = {
+    login: '',
+    email: '',
+    firstName: '',
+    surName: '',
+    hashedPassword: ''
+  }
+
+  constructor(private registerService: AdminService) { }
+
+  ngOnInit(): void {}
+
+  async register(moderator: ModeratorInfo){
+    await this.registerService.registerModerator(moderator)
+    console.log(moderator)
+  }
+
+}
