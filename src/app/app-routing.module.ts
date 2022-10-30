608bfcfca36c23506d54f381361fb0ab060ef6f3
Pawel K <pawelkarabowicz@gmail.com>
2022-10-30 22:23:56 +0100

    feat: Add routes for client and placeowner components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import {AdminComponent} from "./admin/admin.component";
+import {ClientComponent} from "./client/client.component";
+import {PlaceownerComponent} from "./placeowner/placeowner.component";
 
 const routes: Routes = [
-  { path: 'admin', component: AdminComponent},
+  { path: 'placeowner', component: PlaceownerComponent },
+  { path: 'client', component: ClientComponent },
+  { path: 'admin', component: AdminComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: ''}
 ];
