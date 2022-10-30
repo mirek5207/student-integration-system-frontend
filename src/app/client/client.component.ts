6a4508316d5199084989d2802c39b53c86311bd1
Pawel K <pawelkarabowicz@gmail.com>
2022-10-30 22:22:25 +0100

    feat: Add client component

---
+++ src/app/client/client.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {ClientInfo} from "../shared/interfaces/client.interface";
+import {ClientService} from "../shared/services/client/client.service";
+
+
+@Component({
+  selector: 'app-client',
+  templateUrl: './client.component.html',
+  styleUrls: ['./client.component.scss']
+})
+export class ClientComponent implements OnInit {
+
+  registerNewClient: ClientInfo = {
+    login: '',
+    email: '',
+    firstName: '',
+    surName: '',
+    hashedPassword: ''
+  }
+
+  constructor(private clientService : ClientService) { }
+
+  ngOnInit(): void {
+  }
+
+  async register(client: ClientInfo){
+    await this.clientService.registerClient(client)
+    console.log(client)
+  }
+
+}
