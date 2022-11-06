d14a59b805b7aef0714fb882afe50b20b5798c30
Pawel K <pawelkarabowicz@gmail.com>
2022-11-06 15:29:02 +0100

    fix: Rename navigation links for admin

---
+++ src/app/shared/navigation/AminNavLinks.ts
@@ -0,0 +1,15 @@
+export const navlinks: {name: string, url: string}[] = [
+  {
+  name: 'Create moderator',
+  url: 'admin/createModeratorAccount'
+  },
+  {
+    name: 'Get moderators',
+    url: 'admin/getModerators'
+  },
+  {
+    name: 'Reports',
+    url: '/getSystemReporst'
+  },
+];
+
