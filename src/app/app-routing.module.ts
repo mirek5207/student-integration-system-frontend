b9af97a2a62e3b8300ff9649711522fab39f5e66
Pawel K <pawelkarabowicz@gmail.com>
2022-11-06 15:35:44 +0100

    fix: Add new or replace old routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,23 @@ import { LoginComponent } from "./login/login.component";
 import {AdminComponent} from "./admin/admin.component";
 import {ClientComponent} from "./client/client.component";
 import {PlaceownerComponent} from "./placeowner/placeowner.component";
+import {RegisterComponent} from "./register/register.component";
+import {LobbyComponent} from "./lobby/lobby.component";
+import {AdminGetModeratorsComponent} from "./admin/admin-get-moderators/admin-get-moderators.component";
+import {AdminCreateModeratorComponent} from "./admin/admin-create-moderator/admin-create-moderator.component";
 
 const routes: Routes = [
+  { path: 'user/lobbies', component: LobbyComponent},
+  { path: 'createAccount', component: RegisterComponent},
   { path: 'placeowner', component: PlaceownerComponent },
   { path: 'client', component: ClientComponent },
-  { path: 'admin', component: AdminComponent },
+  { path: 'admin',
+    component: AdminComponent,
+    children: [
+      {path: 'getModerators', component: AdminGetModeratorsComponent},
+      {path: 'createModeratorAccount', component: AdminCreateModeratorComponent}
+    ]
+  },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: ''}
 ];
