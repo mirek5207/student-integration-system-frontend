d06dff9d7dff24fe891830d9ba909b9502b3ff7b
Pawel K <pawelkarabowicz@gmail.com>
2022-11-06 15:56:57 +0100

    feat: Add service to get navlinks and redirect to path depends on user role

---
+++ src/app/shared/services/navigation/navigation.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {navlinks as AdminNavLinks} from "../../navigation/AminNavLinks";
+import {navlinks as ModeratorNavLinks} from "../../navigation/moderatorNavLinks";
+import {navlinks as PlaceOwnerNavLinks} from "../../navigation/placeOwnerNavLinks";
+import {navlinks as UserNavLinks} from "../../navigation/userNavLinks";
+import {TokenService} from "../token/token.service";
+import {Router} from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+
+  constructor(private tokenService: TokenService, private router: Router) { }
+
+  getNavLinks(): any{
+    switch(this.tokenService.getRole()) {
+      case 'Admin':
+        return AdminNavLinks
+      case 'Moderator':
+        return ModeratorNavLinks
+      case 'PlaceOwner':
+        return PlaceOwnerNavLinks
+      case 'Client':
+        return UserNavLinks
+      default:
+        return []
+    }
+  }
+
+  async navigate(){
+    switch(this.tokenService.getRole()) {
+      case 'Admin':
+        await this.router.navigateByUrl('/admin');
+        break;
+      case 'Moderator':
+        await this.router.navigateByUrl('/moderator');
+        break;
+      case 'PlaceOwner':
+        await this.router.navigateByUrl('/placeOwenr');
+        break;
+      case 'Client':
+        await this.router.navigateByUrl('/client');
+        break;
+      default:
+        await this.router.navigateByUrl('/login');
+    }
+  }
+}
