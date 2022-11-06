bf76a722023065fb56d507fb5e016a4727ca7aaa
Pawel K <pawelkarabowicz@gmail.com>
2022-11-06 15:52:46 +0100

    feat: Add navlinks for user

---
+++ src/app/shared/navigation/userNavLinks.ts
@@ -0,0 +1,14 @@
+export const navlinks: {name: string, url: string}[] = [
+  {
+    name: 'Create system report',
+    url: '/report'
+  },
+  {
+    name: 'Report user',
+    url: 'user/second'
+  },
+  {
+    name: 'Show lobbies',
+    url: 'user/lobbies'
+  }
+];
