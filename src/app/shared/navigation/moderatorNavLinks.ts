77b4c520216d01cc6f0b54b612ab47c162c938cb
Pawel K <pawelkarabowicz@gmail.com>
2022-11-06 15:44:13 +0100

    fix: Change naming of names and url

---
+++ src/app/shared/navigation/moderatorNavLinks.ts
@@ -0,0 +1,19 @@
+export const navlinks: {name: string, url: string}[] = [
+  {
+    name: 'Create system report',
+    url: '/systemReport'
+  },
+  {
+    name: 'Get reports',
+    url: '/getUserReport'
+  },
+  {
+    name: 'Update status of report',
+    url: '/updateReportStatus'
+  },
+  {
+    name: 'Deactivate account',
+    url: '/deactivateAccount'
+  },
+
+];
