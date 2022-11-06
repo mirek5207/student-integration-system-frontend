2689cbdc2816bad4db6507cee7ee7ec825b99a83
Pawel K <pawelkarabowicz@gmail.com>
2022-11-06 15:34:39 +0100

    feat: Add verification if user is logged in and get proper navliks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "./shared/services/auth/auth.service";
+import {shareReplay} from "rxjs";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'system-integration-system-frontend';
+
+  isAuthenticated$ = this.authService.isAuthenticated$.pipe(shareReplay(1));
+  navItems$ = this.authService.navItems$.pipe(shareReplay(1));
+
+  navigationLinks: {name: string, url:string}[] = []
+
+  constructor(private authService: AuthService) {
+    this.navItems$.subscribe(r => this.navigationLinks = r)
+  }
 }
