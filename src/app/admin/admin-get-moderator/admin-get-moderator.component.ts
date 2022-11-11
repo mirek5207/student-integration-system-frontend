8bd5110b10625792bade68907153c6614ca58745
Pawel K <pawelkarabowicz@gmail.com>
2022-11-11 20:36:27 +0100

    feat: Add load moderator and update moderator functions

--- src/app/admin/admin-get-moderator/admin-get-moderator.component.ts
+++ src/app/admin/admin-get-moderator/admin-get-moderator.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
 import {AdminService} from "../../shared/services/admin/admin.service";
-import {GetModerator} from "../../shared/interfaces/moderator.interface";
-import {Observable} from "rxjs";
+import {GetModerator, UpdateModerator} from "../../shared/interfaces/moderator.interface";
+import {first, Observable} from "rxjs";
 
 @Component({
   selector: 'app-admin-get-moderator',
@@ -10,17 +10,26 @@ import {Observable} from "rxjs";
   styleUrls: ['./admin-get-moderator.component.scss']
 })
 export class AdminGetModeratorComponent implements OnInit {
-
+  moderator!: GetModerator
+  updateModerator: UpdateModerator = {} as UpdateModerator
   moderator$!: Observable<GetModerator>
 
-  constructor(private route: ActivatedRoute, private adminService: AdminService) { }
+  constructor(private route: ActivatedRoute, private adminService: AdminService) {}
 
   id!: number;
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => this.id = params['id'])
-     this.adminService.getModeratorById(this.id).subscribe(r => console.log(r))
+    this.loadModerator()
+    this.moderator$.pipe(first()).subscribe(mod => this.moderator = mod)
+  }
 
+  loadModerator(){
+    this.route.params.subscribe((params: Params) => this.id = params['id'])
+    this.moderator$ = this.adminService.getModeratorById(this.id)
 
   }
+  update(moderator: UpdateModerator){
+    this.adminService.updateModerator(this.id, moderator).subscribe()
+  }
+
 
 }
