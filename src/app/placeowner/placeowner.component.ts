d7569cbc6b94f083b8580c4702d83021715e6615
Pawel K <pawelkarabowicz@gmail.com>
2022-11-12 19:28:35 +0100

    fix: Remove unused items

--- src/app/placeowner/placeowner.component.ts
+++ src/app/placeowner/placeowner.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {PlaceOwnerInfo} from "../shared/interfaces/placeOwner.interface";
-import {RegisterService} from "../shared/services/register/register.service";
-import {AuthService} from "../shared/services/auth/auth.service";
+
 
 @Component({
   selector: 'app-placeowner',
@@ -10,22 +8,9 @@ import {AuthService} from "../shared/services/auth/auth.service";
 })
 export class PlaceownerComponent implements OnInit {
 
-  registerNewPlaceOwner: PlaceOwnerInfo = {
-    login: '',
-    email: '',
-    firstName: '',
-    surName: '',
-    hashedPassword: ''
-  }
-
-  constructor(private placeOwner : RegisterService, private authService : AuthService) { }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-
-  async register(placeOwner: PlaceOwnerInfo){
-    await this.placeOwner.createAccount(placeOwner, 'PlaceOwner/createAccount')
-    await this.authService.loginAfterRegistration()
   }
 
 }
