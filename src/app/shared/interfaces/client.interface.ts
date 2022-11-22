7dfdbea59dfb8ea4c2f5c7f3b6e1467e9d96bce0
Miroslaw Karwowski <mirek5207@gmail.com>
2022-11-22 13:05:26 +0100

    feat: add interface GetClient and UpdateClient

--- src/app/shared/interfaces/client.interface.ts
+++ src/app/shared/interfaces/client.interface.ts
@@ -5,3 +5,31 @@ export interface ClientInfo {
   surName: string,
   hashedPassword: string
 }
+
+export interface GetClient {
+  id: number,
+  firstName: string,
+  surName: string,
+  userId: number,
+  user: {
+    id: number,
+    login: string,
+    email: string,
+    account: {
+      id: number,
+      isActive: boolean,
+      accountCreationTime: string,
+      userId: number,
+      user?: string
+    },
+    placeOwner: boolean,
+  }
+}
+export interface UpdateClient{
+  login: string,
+  email: string,
+  firstName: string
+  surName: string
+  hashedPassword: string,
+  isAccountActive: boolean
+}
