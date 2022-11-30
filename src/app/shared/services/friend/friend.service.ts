e5fc65be8aaf09d07c293166135e7ee2edc66462
Pawel K <pawelkarabowicz@gmail.com>
2022-11-30 15:59:18 +0100

    feat: Add friend componentice

---
+++ src/app/shared/services/friend/friend.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {getUrl, httpOptions} from "../../API/api";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendService {
+
+  constructor(private http: HttpClient) { }
+
+  getFriendships(friendshipId: number){
+    const params = new HttpParams()
+      .set('friendshipId', friendshipId)
+    return this.http.get(getUrl(`Friend/getAllFriendship`), {
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  getAllFriendships(userId: number){
+    const params = new HttpParams()
+      .set('userId', userId)
+    return this.http.get(getUrl(`Friend/getFriendship`), {
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  getAllInvites(userId: number){
+    const params = new HttpParams()
+      .set('userId', userId)
+    return this.http.get(getUrl(`Friend/getAllInvites`), {
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  getAllInvitesClient(userId: number){
+    const params = new HttpParams()
+      .set('userId', userId)
+    return this.http.get(getUrl(`Friend/getAllInvitedClients`), {
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  getAllFriendsToLobby(userId: number, lobbyId: number){
+    const params = new HttpParams()
+      .set('userId', userId)
+      .set('lobbyId', lobbyId)
+    return this.http.get(getUrl(`Friend/getAllFriendsToLobby`), {
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  sendInvite(userId: number, friendUserId: number){
+    const params = new HttpParams()
+      .set('userId', userId)
+      .set('friendUserId', friendUserId)
+    return this.http.post(getUrl(`Friend/sendInvite`),{},{
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  acceptInvite(friendshipId: number){
+    const params = new HttpParams()
+      .set('friendshipId', friendshipId)
+    return this.http.put(getUrl(`Friend/acceptInvite`),{},{
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  rejectInvite(friendshipId: number){
+    const params = new HttpParams()
+      .set('friendshipId', friendshipId)
+    return this.http.delete(getUrl(`Friend/rejectInvite`),{
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+
+  deleteFriend(friendshipId: number){
+    const params = new HttpParams()
+      .set('friendshipId', friendshipId)
+    return this.http.delete(getUrl(`Friend/deleteFriend`),{
+      headers: httpOptions.headers,
+      params: params
+    })
+  }
+}
