510da2aed2463e056df410dd84a0a7736c606aa8
Pawel K <pawelkarabowicz@gmail.com>
2022-12-10 15:42:02 +0100

    feat: Add new method

--- src/app/client/lobbies/create-lobby/create-lobby.component.ts
+++ src/app/client/lobbies/create-lobby/create-lobby.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TokenService} from "../../../shared/services/token/token.service";
 import {LobbyService} from "../../../shared/services/lobby/lobby.service";
+import {PlaceService} from "../../../shared/services/place/place.service";
 
 @Component({
   selector: 'app-create-lobby',
@@ -18,12 +19,19 @@ export class CreateLobbyComponent implements OnInit {
     customPlaceId:  null,
     ownerId: this.tokenService.getId()
   }
-  constructor(private lobbyService: LobbyService, private tokenService: TokenService) { }
+  constructor(private lobbyService: LobbyService,
+              private tokenService: TokenService,
+              private placeService: PlaceService) { }
 
   ngOnInit(): void {
+    this.getAllPlaces()
   }
 
   createLobby(lobby: any){
     this.lobbyService.createLobby(lobby);
+  }
+
+  getAllPlaces(){
+     this.placeService.getPlaces().subscribe(r => this.locals = r);
   }
 }
